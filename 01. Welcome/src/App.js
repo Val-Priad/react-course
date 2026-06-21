9c8bf892046138f1dd4d7780d34f5de269b7a7be
Val-Priad <valeriipriadchenko@gmail.com>
2026-06-21 21:26:18 +0200

    changed styling a bit and added an entry point

--- 01. Welcome/src/App.js
+++ 01. Welcome/src/App.js
@@ -2,24 +2,29 @@ import { useState } from "react";
 
 function App() {
   const [advice, setAdvice] = useState("");
+  const [fetching, setFetching] = useState(false);
   let [adviceCount, setAdviceCount] = useState(0);
 
   async function getAdvice() {
+    setFetching(true);
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
     setAdviceCount(++adviceCount);
     console.log(data);
+    setFetching(false);
   }
 
-  // useEffect(() => {
-  //   getAdvice();
-  // }, []);
-
   return (
-    <div>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice}>Get advice!</button>
+    <div className="max-w-4xl w-full flex justify-center items-center flex-col bg-stone-100 rounded-xl p-5 shadow-lg">
+      <h1 className="text-green-950 font-semibold text-xl mb-6">{advice}</h1>
+      <button
+        className="bg-green-500 hover:bg-green-600 shadow-md disabled:bg-gray-400 rounded-xl text-white py-1 px-3 rounded-m text-lg mb-3"
+        disabled={fetching}
+        onClick={getAdvice}
+      >
+        Get advice!
+      </button>
       <Message count={adviceCount}></Message>
     </div>
   );
@@ -27,7 +32,7 @@ function App() {
 
 function Message(props) {
   return (
-    <p>
+    <p className="text-gray-600 text-sm">
       You have read <strong>{props.count}</strong> pieces of advice:)
     </p>
   );
